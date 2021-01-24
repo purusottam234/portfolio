8326ba64131f8ae4edf01bc419f68be00223393a
Purusottam <purusottamadhikari234@gmail.com>
2021-01-24 21:06:27 +0545

    blog views created

--- personal_portfolio/blog/admin.py
+++ personal_portfolio/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post, Category
+
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Post)
+admin.site.register(Category)
--- personal_portfolio/blog/views.py
+++ personal_portfolio/blog/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Post, Comment
+
+
+def blog_index(request):
+    posts = Post.object.all().order_by('-created_on')
+    context = {
+        "post": posts
+    }
+    return render(request, 'blog_index.html', context)
+
+
+def blog_category(request, category):
+    posts = Post.objects.filter(
+        categories_name_contains=category
+        ).order_by('-created_on')
+    )
+    context={
+        'category': category,
+        'posts': posts
+    }
+    return render(request, 'blog_category.html', context)
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+    }
+
+    return render(request, "blog_detail.html", context)
