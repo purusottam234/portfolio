58877280cf0480a51cbf27e4dca60013862a0e9f
Purusottam <purusottamadhikari234@gmail.com>
2021-01-25 13:05:05 +0545

    changes in the form

--- personal_portfolio/blog/forms.py
+++ personal_portfolio/blog/forms.py
@@ -2,15 +2,16 @@ from django import forms
 
 
 class CommentForm(forms.Form):
-    auther = forms.CharField(
-        max_length=60, widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'yourname'
+    author = forms.CharField(
+        max_length=60,
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "placeholder": "Your Name"
         })
     )
     body = forms.CharField(widget=forms.Textarea(
         attrs={
-            'class': "form-control",
-            'placeholder': 'leave a Comment'
+            "class": "form-control",
+            "placeholder": "Leave a comment!"
         })
     )
